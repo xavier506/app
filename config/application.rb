b00e9aa8795fb9675bfe4b8c2fd25766b6030aee
Xavier Fernandez <xavier.fernandez@annkissam.com>
2016-12-09 21:17:01 -0600

    Add Icons, Table Format, Translations, Seed Data

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,9 @@ module Pcalogistica
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    #
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.default_locale = :en
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.new(
+  email: 'gardella@pcalogistica.com',
+  password: 'sexavier69',
+  password_confirmation: 'sexavier69',
+  admin: true
+)
+user.save!
