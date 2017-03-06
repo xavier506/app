34dfefe9dcb066f69dc92e241506ea2e4b61e1c7
Xavier Fernandez <xavier@crservers.com>
2017-03-06 10:16:54 -0600

    Add calculations for container models and pre populate values
    - update gems

--- app/models/order.rb
+++ app/models/order.rb
@@ -13,12 +13,38 @@ class Order < ApplicationRecord
   MODES = ['Ocean / Maritimo','Land','Air', 'Export', 'Import', 'Other']
   STATUSES = ['Open','Closed','Void']
 
+  def total_containers(order_id)
+    Container.where(order_id: order_id).count
+  end
+
   def total_units(order_id)
     Container.where(order_id: order_id).sum(:units)
   end
 
   def total_weight(order_id)
     Container.where(order_id: order_id).sum(:gross_weight)
+  end
+
+  def total_volume(order_id)
+    Container.where(order_id: order_id).sum(:volume)
+  end
+
+  def unit_type(order_id)
+    if Container.where(order_id: order_id).exists?
+      Container.where(order_id: order_id).first.unit_type
+    end
+  end
+
+  def weight_units(order_id)
+    if Container.where(order_id: order_id).exists?
+      Container.where(order_id: order_id).first.weight_units
+    end
+  end
+
+  def volume_units(order_id)
+    if Container.where(order_id: order_id).exists?
+      Container.where(order_id: order_id).first.volume_units
+    end
   end
 
   validates :client, presence: true
